293279f37f950a592010f6f3faf01bbfbc4619af
teamgraphicdmm-sys <teamgraphic.dmm@educul.in>
2026-07-13 16:08:19 +0530

    tag added

--- wp-content/themes/homeland-infinia/header.php
+++ wp-content/themes/homeland-infinia/header.php
@@ -8,6 +8,16 @@
     <meta property="og:type" content="website">
     <meta property="og:title" content="<?php bloginfo('name'); ?>">
     <meta property="og:url" content="<?php echo esc_url(home_url('/')); ?>">
+    <meta name="google-site-verification" content="jIsOVQsWxv7w_RSoL3Ni0PLbd1kS8B4UG4WSB_DmK90" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F3FP2QJQSM"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-F3FP2QJQSM');
+    </script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,7 +36,7 @@
 
     <div class="header-center">
         <a href="<?php echo esc_url(home_url('/')); ?>" class="brand-logo-link">
-            <img class="brand-logo-img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/infinia-logo.png'); ?>" alt="Homeland Infinia Logo">
+            <img class="brand-logo-img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/Homeland_Infinia_light.png'); ?>" alt="Homeland Infinia Logo">
         </a>
     </div>
 
