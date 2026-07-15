1718f8a30f145eaf662c79b1cb1403db4dab1fbd
teamgraphicdmm-sys <teamgraphic.dmm@educul.in>
2026-07-15 11:09:26 +0530

    hero section title added

--- wp-content/themes/homeland-infinia/front-page.php
+++ wp-content/themes/homeland-infinia/front-page.php
@@ -18,13 +18,34 @@ get_header();
     background-clip: text;
     -webkit-text-fill-color: transparent;
 }
+.small-title {
+    font-family: "Aboreto", system-ui;
+    font-weight: 500;
+    color: #ffffff;
+    display: block;
+    font-size: clamp(16px, 2.5vw, 25px);
+    margin-bottom: -1%; 
+    margin-left: -45%;
+}
+@media (max-width: 992px) {
+    .small-title {
+            margin-bottom: -3%; 
+         margin-left:0% !important;
+    }
+}
+@media (max-width: 768px) {
+       .small-title {
+        margin-bottom: -4%; 
+    } 
+}
 </style>
 <video class="bg-video-layer" src="<?php echo esc_url(get_template_directory_uri() . '/assets/video/homeland_bg_video.mp4'); ?>" autoplay loop muted playsinline webkit-playsinline></video>
 
 <div id="app-container">
     <!-- <div class="content-block"> -->
         <div class="text-block">
-            <h1>Coming soon!</h1>
+            <span class="small-title">To be</span>
+            <h1>unveiled</h1>
             <span class="badge2">Newest Story of Chandigarh</span>
             <div class="divider-logo">
                 <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/homeland-logo-icon.png'); ?>" alt="" style="height:25px;">
